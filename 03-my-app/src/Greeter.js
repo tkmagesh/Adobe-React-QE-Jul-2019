228a4e1cf157b9b93b680f480a481b9669ea766a
Magesh <tkmagesh77@gmail.com>
2019-07-13 12:18:05 +0530

    app setup

---
+++ 03-my-app/src/Greeter.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+class Greeter extends React.Component{
+	
+	state = { 
+		message : '[Dummy message]',
+		userName : ''
+	};
+
+	onBtnGreetClick = () => {
+		console.log(this.props);
+		this.setState({ message : `Hi ${this.state.userName}, ${this.props.msg}`});
+	}
+
+	onTxtUserNameChange = (evt) => {
+		this.setState({ userName : evt.target.value });
+	}
+
+	render(){
+		
+		return(
+			<div>
+				<h1>Greeter</h1>
+				<hr/>
+				<label htmlFor="">User Name :</label>
+				<input type="text" onChange={this.onTxtUserNameChange} />
+				<input type="button" value="Greet" onClick={this.onBtnGreetClick} />
+				<div className="highlight">{this.state.message}</div>
+			</div>
+		)
+	}
+}
+
+export default Greeter;
